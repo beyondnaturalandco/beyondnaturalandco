84242475ddfdc859113393281c5a3d93cbf74bac
Dann-13 <dannyelgomezz.13@gmail.com>
2024-06-27 18:47:10 -0500

    Animando secciones

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,26 +1,47 @@
 import React from "react";
 import "./ExploreMenu.css";
 import { menu_list } from "../../assets/assets";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+import MenuItem from "../MenuItem/MenuItem";
 
-const ExploreMenu = ({category,setCategory}) => {
-
+const ExploreMenu = ({ category, setCategory }) => {
+  const [ref, inView] = useInView({
+    triggerOnce: true, // Para activar la animación una vez cuando entre en la vista
+    threshold: 0.5, // Porcentaje de visibilidad para activar la detección
+  });
   return (
-    <div className="explore-menu" id="explore-menu">
-      <h1>Explore our menu</h1>
-      <p className="explore-menu-text">
-        Choose from a diverse menu featuring a delectable array of dishes.Our mission is to satisfy your cravings and elevate your dining experience.one delicious meal at a time.
-      </p>
+    <div ref={ref} className={`explore-menu ${inView ? "visible" : ""}`} id="explore-menu">
+      <motion.h1
+        initial={{ y: 20, opacity: 0 }}
+        animate={inView ? { y: 0, opacity: 1 } : {}}
+        transition={{ duration: 0.5 }}
+      >
+        Explore our menu
+      </motion.h1>
+      <motion.p
+        className="explore-menu-text"
+        initial={{ y: 20, opacity: 0 }}
+        animate={inView ? { y: 0, opacity: 1 } : {}}
+        transition={{ duration: 0.5, delay: 0.2 }}
+      >
+        Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.
+      </motion.p>
       <div className="explore-menu-list">
-        {menu_list.map((item, index) => {
-          return (
-            <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
-              <img loading="lazy" className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
-              <p>{item.menu_name}</p>
-            </div>
-          );
-        })}
+        {menu_list.map((item, index) => (
+          <MenuItem
+            key={index}
+            item={item}
+            category={category}
+            setCategory={setCategory}
+          />
+        ))}
       </div>
-      <hr />
+      <motion.hr
+        initial={{ y: 20, opacity: 0 }}
+        animate={inView ? { y: 0, opacity: 1 } : {}}
+        transition={{ duration: 0.5, delay: 0.6 }}
+      />
     </div>
   );
 };
--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -2,18 +2,28 @@ import React, { useContext } from "react";
 import "./FoodDisplay.css";
 import FoodItem from "../FoodItem/FoodItem";
 import { StoreContext } from "../../context/StoreContext";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
 
   return (
-    <div className="food-display" id="food-display">
+    <motion.div
+      ref={ref}
+      className={`food-display ${inView ? "visible" : ""}`}
+      id="food-display"
+      initial={{ y: 20, opacity: 0 }}
+      animate={inView ? { y: 0, opacity: 1 } : {}}
+      transition={{ duration: 0.5 }}
+    >
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => {
-          {
-            console.log(category, item.category);
-          }
           if (category === "All" || category === item.category) {
             return (
               <FoodItem
@@ -25,10 +35,10 @@ const FoodDisplay = ({ category }) => {
               />
             );
           }
+          return null;
         })}
       </div>
-    </div>
+    </motion.div>
   );
 };
-
 export default FoodDisplay;
