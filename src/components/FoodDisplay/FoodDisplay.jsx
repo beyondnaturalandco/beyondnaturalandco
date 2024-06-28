81fc764820f67ee4191ef9b4b91134c795592a3b
Dann-13 <dannyelgomezz.13@gmail.com>
2024-06-28 15:21:13 -0500

    animando items

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,40 +3,34 @@ import React, { useContext } from "react";
 import "./FoodDisplay.css";
 import FoodItem from "../FoodItem/FoodItem";
 import { StoreContext } from "../../context/StoreContext";
-import { motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
-  const [ref, inView] = useInView({
-    triggerOnce: true, // Para activar la animación una vez cuando entre en la vista
-    threshold: 0.5, // Porcentaje de visibilidad para activar la detección
-  });
-  return (
-    <div ref={ref} className={`food-display ${inView ? "visible" : ""}`} id="food-display">
 
+
+  return (
+    <div className='food-display' id="food-display">
       <h2>Top dishes near you</h2>
-      <motion.div className="food-display-list" initial={{ y: 20, opacity: 0 }}
-        animate={inView ? { y: 0, opacity: 1 } : {}}
-        transition={{ duration: 0.5 }}>
+      <div className="food-display-list">
         {food_list.map((item, index) => {
           if (category === "All" || category === item.category) {
             return (
-              <FoodItem
+              <div
                 key={index}
-                id={item._id}
-                name={item.name}
-                description={item.description}
-                image={item.image}
-              />
+              >
+                <FoodItem
+                  id={item._id}
+                  name={item.name}
+                  description={item.description}
+                  image={item.image}
+                />
+              </div>
             );
           }
           return null;
         })}
-      </motion.div>
-
+      </div>
     </div>
-
   );
 };
 
--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./FoodItem.css";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const FoodItem = ({ id, name, description, image }) => {
-
   return (
-    <div className="food-item">
+    <motion.div className="food-item"
+      whileHover={{ y: -10 }} // Animación cuando el usuario pasa el mouse
+      whileTap={{ scale: 0.95 }} // Escala cuando se toca en dispositivos móviles
+    >
       <div className="food-item-img-container">
         <img loading="lazy" className="food-item-image" src={image} alt="" />
       </div>
@@ -17,8 +20,7 @@ const FoodItem = ({ id, name, description, image }) => {
           <p className="food-item-desc">{description}</p>
         </div>
       </Link>
-
-    </div>
+    </motion.div>
   );
 };
 
