cd985c7b4ac866dd6b689875987cec729c9c5515
JuanFeSC <juanf.castro.94@gmail.com>
2024-07-04 19:37:03 -0500

    Cambios mision,vision, logo instagram al final y cambio texto al final

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -13,14 +13,14 @@ const AboutMe = () => {
                 animate={inView ? { y: 0, opacity: 1 } : {}}
                 transition={{ duration: 0.5 }}>
                 <h2>All About us</h2>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam natus totam nemo laborum dolorum in est, illo nam, neque ipsam quidem corporis facere, quo doloribus! Commodi autem ex recusandae reprehenderit.</p>
+                <p>Beyond Natural and Co is a family-owned business dedicated to cultivating a genuine love for nourishing foods that power your body. Clean eating is about learning to love the food you eat and the way it fuels you. We believe this approach leads to sustainable lifestyle changes.</p>
             </motion.div>
             <motion.div className="about_our" initial={{ y: 20, opacity: 0 }}
                 animate={inView ? { y: 0, opacity: 1 } : {}}
                 transition={{ duration: 0.5 }}>
                 <div className="about_our_story">
                     <h2>Our Story</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil suscipit ab error, impedit eaque id? Sunt aliquid facere temporibus exercitationem ipsam dolore aliquam saepe magni perspiciatis. Excepturi, dolorem quam temporibus ex ipsa consequuntur quod aperiam, dolor error itaque autem voluptate nemo consectetur laborum officiis, molestias illum alias veniam. Perferendis, commodi.</p>
+                    <p>The story of Beyond Natural and Co began with a simple idea: to create a family-owned business dedicated to cultivating a genuine love for nourishing foods that power your body. From the very beginning, clean eating has been about learning to love the food you eat and the way it fuels you. This belief has guided us towards promoting sustainable lifestyle changes. Our journey has led us to offer a diverse range of options, whether your goal is weight loss, overall health improvement, or enhancing fitness. We prioritize quality by focusing on organic produce and well-balanced macro-friendly meals. Our vibrant salads, hearty grain bowls, and customizable options are designed to make healthy eating enjoyable and stress-free. We’ve extended our offerings to include convenient, nutritious snacks and breakfast choices, as well as delightful sweet and savory crepes. Our mission remains unchanged: to bridge the gap between fast, convenient food and wholesome nourishment.</p>
                 </div>
                 <div className="about_our_image">
                     <img src={assets.logo} alt="logos" />
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Footer.css'
 import { assets } from '../../assets/assets'
-import { CiFacebook } from "react-icons/ci";
+import { CiFacebook, CiInstagram } from "react-icons/ci";
 
 const Footer = () => {
   return (
@@ -11,21 +11,18 @@ const Footer = () => {
           <div className="footer-content-logo">
             <img src={assets.logowhite} alt="" />
           </div>
-          <p>When you walk in to work every day, you will find that the energy on the floor is just amazing at Janosa. The passion, the hunger, the drive to excel is so infectious, it brings the best out in everyone.</p>
+          <p>At Beyond Natural and Co, the passion for nourishing foods and healthy living is palpable. Our dedication to quality and innovation fuels an inspiring atmosphere that brings out the best in everyone.</p>
           <div className="footer-social-icons">
             <a href='https://www.facebook.com/profile.php?id=61556948297729&mibextid=ZbWKwL' target='_blank' rel='noopener noreferrer'>
               <CiFacebook size={50} />
             </a>
 
+            <a href='https://www.instagram.com/beyond.natural.co/?igsh=MTAzMGxidmE1anh4eQ%3D%3D' target='_blank' rel='noopener noreferrer'>
+              <CiInstagram size={50} />
+            </a>
+
 
           </div>
-        </div>
-        <div className="footer-content-center">
-          <h2>Beyond</h2>
-          <ul>
-            <li><a href='#'>Home</a></li>
-            <li>About</li>
-          </ul>
         </div>
         <div className="footer-content-right">
           <h2>GET IN TOUCH</h2>
