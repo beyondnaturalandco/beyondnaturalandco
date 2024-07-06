9d289f96c490394bee3268ffae593b0192956be1
Dann-13 <dannyelgomezz.13@gmail.com>
2024-07-05 20:19:47 -0500

    Mostrando pdf menu

---
+++ src/components/menuPdf/MenuPdf.jsx
@@ -0,0 +1,27 @@
+import "./MenuPdf.css";
+import Pdf from '../../assets/Pdf/Menu.pdf'
+
+const MenuPdf = () => {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    const handleDownload = () => {
+        window.open(Pdf, '_blank');
+    };
+    return (
+        <div className="pdf-container">
+            {!isMobile ? (
+                <iframe
+                    src={Pdf}
+                    className="pdf-iframe"
+                    title="Menu PDF"
+                />
+            ) : (
+                <button className="download-button" onClick={handleDownload}>
+                    Descargar Menú
+                </button>
+            )}
+        </div>
+    );
+};
+
+export default MenuPdf;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,16 +4,16 @@ import Header from "../../components/Header/Header";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 import AboutMe from "../../components/AboutMe/AboutMe";
-
+import MenuPdf from "../../components/menuPdf/MenuPdf";
 const Home = () => {
   const [category, setCategory] = useState("All");
-
   return (
     <div>
       <Header />
       <AboutMe />
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category} />
+      <MenuPdf />
     </div>
   );
 };
