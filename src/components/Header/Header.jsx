e1b91e0bffaacd8310cc127fedce4884d0e6b83d
beyondnaturalandco <beyondnaturalandco@gmail.com>
2026-09-11 12:38:45 -0400

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./Header.css";
-import menuPdf from "../../assets/Pdf/Menu beyon.pdf";
 
 const Header = () => {
-  const [bgImageLoaded, setBgImageLoaded] = useState(false);
-
-  useEffect(() => {
-    const bgImage = new Image();
-    bgImage.src =
-      "https://raw.githubusercontent.com/beyondnaturalandco/beyondnaturalandco/main/public/header_img.webp";
-
-    bgImage.onload = () => setBgImageLoaded(true);
-  }, []);
-
   return (
-    <div className={`header ${bgImageLoaded ? "loaded" : ""}`}>
+    <header className="header">
+      <div className="header-overlay"></div>
+
       <div className="header-contents">
+        <h1>Fresh • Healthy • Natural</h1>
+
+        <p>
+          Fresh, balanced meals made with quality ingredients.
+          Eat better. Feel better. Go Beyond.
+        </p>
+
         <a
-          href={menuPdf}
+          href="https://beyondnaturalandco.cloveronline.com/"
           target="_blank"
           rel="noopener noreferrer"
+          className="header-button"
         >
-          View Menu
+          ORDER NOW
         </a>
       </div>
-    </div>
+    </header>
   );
 };
 
