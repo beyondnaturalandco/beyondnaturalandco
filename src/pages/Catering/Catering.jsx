a1c8f23599d4a88f6d0609442d70767db15b4145
beyondnaturalandco <beyondnaturalandco@gmail.com>
2026-09-24 16:05:46 -0400

    Add catering page

---
+++ src/pages/Catering/Catering.jsx
@@ -0,0 +1,14 @@
+import "./Catering.css";
+
+const Catering = () => {
+  return (
+    <main className="catering-page">
+      <div className="catering-page__content">
+        <h1>Catering</h1>
+        <p>Catering options are coming soon.</p>
+      </div>
+    </main>
+  );
+};
+
+export default Catering;
